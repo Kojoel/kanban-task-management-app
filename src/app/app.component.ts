d4d68146be9f69df539e19c0361971c5b5afefb4
Kojoel <nanaabruquah1s@gmail.com>
2024-09-17 16:52:29 +0000

    Board component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { BoardComponent } from './components/board/board.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SideBarComponent],
+  imports: [RouterOutlet, HeaderComponent, SideBarComponent, BoardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
