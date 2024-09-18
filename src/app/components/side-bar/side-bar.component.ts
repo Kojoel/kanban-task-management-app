0d0b01e0a998a5db2cfff82a6ea31b0413b648e9
Kojoel <nanaabruquah1s@gmail.com>
2024-09-18 11:52:01 +0000

    Toggle sidebar service implemented

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SidebarService } from '../../services/sidebar/sidebar.service';
 
 @Component({
   selector: 'app-side-bar',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './side-bar.component.scss'
 })
 export class SideBarComponent {
+
+  constructor(public sidebar: SidebarService) {}
 
 }
