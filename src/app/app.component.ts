2523060f211edbff44b029bdcf8248f812bb01e2
Kojoel <nanaabruquah1s@gmail.com>
2024-09-20 09:33:21 +0000

    Added task modal to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { BoardComponent } from './components/board/board.component';
 import { Store } from '@ngrx/store';
 import { loadBoards } from './store/actions/boards.action';
+import { ViewTaskModalComponent } from './components/view-task-modal/view-task-modal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SideBarComponent, BoardComponent,],
+  imports: [RouterOutlet, HeaderComponent, SideBarComponent, BoardComponent, ViewTaskModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
